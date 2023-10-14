e306ee9a90539a9904e69362beb6806ab2a3422c
MichaelVieira021 <michael21rj@gmail.com>
2023-10-14 02:48:20 -0300

    [START] Projeto API
    Grupo 04

---
+++ Jemn/src/main/java/br/com/ecommerce/Jemn/JemnCosmeticsApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecommerce.Jemn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JemnCosmeticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JemnCosmeticsApplication.class, args);
+	}
+
+}

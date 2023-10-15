472f9594735e1eea93ba0d5afbc48e8e9ac2efda
EduardoAngelo1 <eduardoangelo300@gmail.com>
2023-10-15 17:11:07 -0300

    [FIX] resolvendo problemas no vs code

--- Jemn/src/main/java/br/com/ecommerce/Jemn/dto/categoria/CategoriaRequestDTO.java
+++ Jemn/src/main/java/br/com/ecommerce/Jemn/dto/categoria/CategoriaRequestDTO.java
@@ -1,10 +1,24 @@
 package br.com.ecommerce.jemn.dto.categoria;
 
+import java.util.List;
+
+import br.com.ecommerce.jemn.model.Produto;
+
 public class CategoriaRequestDTO {
 
     private String nomeCategoria;
     private String obsCategoria;
+     private List<Produto> produtos;
 
+
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
 
     public String getNomeCategoria() {
         return nomeCategoria;
